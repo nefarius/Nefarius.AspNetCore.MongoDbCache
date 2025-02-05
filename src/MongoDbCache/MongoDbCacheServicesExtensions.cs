3c96804035a5d4cf009310a73aab1e66ff2e10a6
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-05 23:49:26 +0100

    Added AddMongoDbCache overload

--- src/MongoDbCache/MongoDbCacheServicesExtensions.cs
+++ src/MongoDbCache/MongoDbCacheServicesExtensions.cs
@@ -3,12 +3,15 @@ using System.Diagnostics.CodeAnalysis;
 
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 
 namespace Nefarius.AspNetCore.MongoDbCache;
 
 /// <summary>
-///     <see cref="IServiceCollection"/> extensions.
+///     <see cref="IServiceCollection" /> extensions.
 /// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public static class MongoDbCacheServicesExtensions
 {
     /// <summary>
@@ -36,7 +39,39 @@ public static class MongoDbCacheServicesExtensions
 
         services.AddOptions();
         services.Configure(setupAction);
-        services.Add(ServiceDescriptor.Singleton<IDistributedCache, MongoDbCache>());
+        services.AddSingleton<IDistributedCache, MongoDbCache>();
+
+        return services;
+    }
+
+    /// <summary>
+    ///     Adds MongoDb distributed caching services to the specified <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+    /// <param name="setupAction">
+    ///     An <see cref="Action{MongoDbCacheOptions,IServiceProvider}" /> to configure the provided
+    ///     <see cref="MongoDbCacheOptions" />.
+    /// </param>
+    /// <returns>The <see cref="IServiceCollection" /> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddMongoDbCache(
+        this IServiceCollection services,
+        Action<MongoDbCacheOptions, IServiceProvider> setupAction)
+    {
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        if (setupAction == null)
+        {
+            throw new ArgumentNullException(nameof(setupAction));
+        }
+
+        services.AddOptions();
+        services.AddSingleton<IConfigureOptions<MongoDbCacheOptions>>(sp =>
+            new ConfigureOptions<MongoDbCacheOptions>(options => setupAction(options, sp))
+        );
+        services.TryAddSingleton<IDistributedCache, MongoDbCache>();
 
         return services;
     }
